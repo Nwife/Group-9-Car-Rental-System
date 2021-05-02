5a8191d85b70190659fa0aa507ee9a8bfa9938ad
Nwife Amaeze <ifeeamaeze@gmail.com>
2021-05-02 17:09:33 +0100

    Create mainheader.php
    made changes to the nav section

---
+++ main/mainheader.php
@@ -0,0 +1,43 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="mainheader.css">
+    <title>Group IX</title>
+</head>
+<body>
+
+    <header>
+        <div class="logo"><a href="index.php"> Group IX</a></div>
+
+        <nav>
+            <ul>
+                <li><a href="aboutus.php">ABOUT US</a></li>
+                <li><a href="contactus.php">CONTACT US</a></li>
+                <li><img class="image" src="profile-user.png">
+                
+                <?php
+                    if(isset($_SESSION['id'])){
+                        echo "<span> Hello, " . $_SESSION['name'];
+                    }else{
+                        echo "<span> Hello, USER";
+                    }
+                ?>
+                </li>
+                <li>
+                <?php if(isset($_SESSION['id'])){
+                    echo "<a id='logout' href='#'>LOGOUT</a>";
+                }else{
+                    echo "<a id='logout' href='#'>RETURN</a>";
+                }
+                ?>
+                </li>
+                <!-- <li><a id="logout" href="#">LOGOUT</a></li>
+                <li><a id="logout" href="#">LOGOUT</a></li> -->
+            </ul>
+        </nav>
+
+    </header>
