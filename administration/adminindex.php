671c6f453a44c6ab28f843a00527ebf399c060d9
Nwife Amaeze <ifeeamaeze@gmail.com>
2021-05-02 22:17:04 +0100

    Update adminindex.php
    Added the php for the registered users cards

--- administration/adminindex.php
+++ administration/adminindex.php
@@ -1,6 +1,19 @@
 
 <?php echo "<link rel='stylesheet' href='adminindex.css'>" ?>
 
+<?php
+
+    require('../includes/dbh.inc.php');
+
+    //write sql to fetch users
+    $sql = "SELECT * FROM users";
+
+    //query the sql
+    $resultUser = mysqli_query($conn, $sql);
+
+    //return as associative array
+    $rowUser = mysqli_fetch_all($resultUser, MYSQLI_ASSOC);
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -24,7 +37,7 @@
                             <path d="M9 14C6.23858 14 4 16.2386 4 19C4 20.6569 5.34315 22 7 22H17C18.6569 22 20 20.6569 20 19C20 16.2386 17.7614 14 15 14H9Z" fill="#fff"/>
                             <path d="M12 2C9.23858 2 7 4.23858 7 7C7 9.76142 9.23858 12 12 12C14.7614 12 17 9.76142 17 7C17 4.23858 14.7614 2 12 2Z" fill="#fff"/>
                         </svg>
-                        <span>8</span> 
+                        <span><?php echo count($rowUser) ?></span> 
                     </div>
                     <div class="innertext"><p>REGISTERED USERS</p></div>
                 </div>
