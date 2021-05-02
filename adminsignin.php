9f8941b9b4e37d4e0bf95790b4e053c1223f117f
Nwife Amaeze <ifeeamaeze@gmail.com>
2021-05-02 22:11:59 +0100

    front end php changes
    Changed the img to svg in mainheader.php
    Added the php for the table in registeredusers.php
    Did form changes in adminsignin.php
    Added the phph for the registered users card in adminindex.php

--- adminsignin.php
+++ adminsignin.php
@@ -6,13 +6,26 @@
             <h1>SIGN IN AS ADMIN</h1>
         </div>
 
+
         <!-- <div class="formparent"> -->
-            <form>
-                <label for="Email" class="label">EMAIL</label><br>
-                <input type="email" class="input-text" name="Email" placeholder=""><br>
+            <form action="includes/adminlogin.inc.php" method="POST">
+            <?php
+
+            if(isset($_GET['error'])){
+                if($_GET['error'] == "emptyfields"){
+                    echo '<p class ="signuperror"> Fill in all fields!</p>';
+                }
+                elseif($_GET['error'] == "wrongnamepwd"){
+                    echo '<p class ="signuperror"> Invalid username or password!</p>';
+                }
+            }
+
+            ?>
+                <label for="name" class="label">USERNAME</label><br>
+                <input type="text" class="input-text" name="adminname" placeholder=""><br>
                 <label for="Password" class="label">PASSWORD</label><br>
-                <input type="password" class="input-text" name="Password"><br>
-                <input type="submit" value="SIGN IN" class="submit-button"><br>
+                <input type="password" class="input-text" name="adminpwd"><br>
+                <input type="submit" value="SIGN IN" class="submit-button" name="adminlogin-submit"><br>
             </form>
         <!-- </div> -->
 
